e9e2a9bfd346de4029e9b1178067c29116ff84fb
Rachael D Mueller <rdmseas@uw.edu>
2022-06-29 16:30:44 -0700

    find nearest node developed and tested function for finding nearest node when given either a single lat/lon value or an array of values. Added function to ssm_utils.py.  Haversine function still included.

--- scripts/ssm_utils.py
+++ scripts/ssm_utils.py
@@ -1,53 +1,81 @@
 import numpy
 import geopandas as gpd
 from shapely.geometry import Point
-from pyproj import CRS
+from pyproj import CRS, Transformer
+
 
 def estimate_nearest_node(shapefile_path, ilat=48.724,ilon=-122.576):
+def find_closest_node(
+    lats=48.724,
+    lons-122.576,
+    shapefile):
     """
-    Calculate the great circle distance in kilometers between two points 
-    on the earth (specified in decimal degrees) using Haversine function
-    
-    Default ilat, ilon values correspond to the Bellingham Bay outfall buoy 
-    (NOAA Station 46118)
-    
-    This script can only run on Hyak and has a hard-coded access to Kevin's shapefile
-    
-    lat: latitude in decimal degree, e.g. 48.724
-    lon: longitude in decimal degree, e.g. -122.576
+    INPUTS:
+        - lats: scalar value or numpy array values 
+        - lons: scalar value or numpy array of values
+        - shapefile (.shp): path and .shp file name, combined
+    OUTPUTS:
+        - node_id: SSM node corresponding to specified lat/lon pair(s), 
+            and same length as input, returned as a list 
+        - index: dataframe row index for nodes.  Same length as input, 
+            returned as a list. 
+        - station_x, station_y: projected lat/lon station locations 
+            (for graphing purposes)
+    DEVELOPEMENT NOTES:
+        Use of Transformer and example of using Pythagorean Theorem instead
+        of Haversine Function are adopted from Su Kyong's "find_closest_node.py"
+        script (https://github.com/RachaelDMueller/KingCounty-Rachael/blob/main/SuKyongs_python/find_closest_node_index.py)
+        
+        QAQC results show examples where the nearest node is different
+        than I would expect (see, e.g. BHAM-bay location 48.767422 N, 
+        -122.575792 E).  This difference is likely due to the way the node
+        center is identified. 
+        
+        This code is designed around a shapefile provided by Kevin Bogue
+        (https://github.com/RachaelDMueller/KingCounty-Rachael/tree/main/kevin_shapefiles/SSMGrid2_062822)
+        It requires attribute names of "lat","lon", and "node_id". 
     """
-    try: 
-        gdf = gpd.read_file(shapefile_path)
-    except FileNotFoundError:
-        print(f'File does not exist: {shapefile_path}')
-    
-    # project lat/lon to shapefile coordinate
-    iloc = [Point(xy) for xy in zip([ilon],[ilat])]
-    crs = CRS('epsg:3857')
-    #crs = CRS('epsg:6318')
-    geo_df_iloc = gpd.GeoDataFrame(geometry = iloc, crs = crs)
-    geo_df_iloc = geo_df_iloc.to_crs(crs = gdf.crs)
-    
-    # find nearest polygon to point
-    polygon_index = gdf.distance(geo_df_iloc).sort_values().index[0]
-    print(polygon_index)
-    nearest_node = gdf['node_id'].loc[polygon_index]
-#     node = numpy.array(gdf['node_id'])
-#     ssmlat,ssmlon = gdf['lat'].values, gdf['lon'].values
-    
-#     # calculate distance using haversine function
-#     dlat = numpy.radians(ssmlat - ilat)
-#     dlon = numpy.radians(ssmlon - ilon)
-#     r = 6367 # approx. radius of earth in km
-#     d = (numpy.sin(dlat/2)**2) + numpy.cos(ilat)*numpy.cos(ssmlat)*(numpy.sin(dlon/2))**2
-#     dist = 2*r*numpy.arcsin(numpy.sqrt(d))
-    
-#     # get the nearest node to input lat/lon pair
-#     nearest_node = node[dist == min(dist)].item()
-#     print(ssmlat[node==nearest_node], ssmlon[node==nearest_node])
-    
-    return nearest_node
-
+    # load shapefile
+    gdf = gpd.read_file(shapefile)
+    # get shapefile projection information
+    shapefile_EPSG = gdf.crs.to_epsg()
+    # create transformation from WGS84 to shapefile projection
+    transformer = Transformer.from_crs('WGS84',f'EPSG:{shapefile_EPSG}')
+    # transform lat(s) and lon(s) to shapefile projection (in meters)
+    stations_y, stations_x=transformer.transform(lats, lons) 
+    # create vector of SSM model lats and lons 
+    # projected to shapefile projection (in meters)
+    ssm_y, ssm_x=transformer.transform(gdf.lat, gdf.lon) 
+    # calculate distance(s) between lat/lon location(s) and model nodes
+    [n_nodes,n_attrs]=gdf.shape
+    index=[]
+    node_id=[]
+    try: # for array of station locations
+        # find nearest node index and ID
+        for idx in range(0,len(stations_y)):
+            distance=(np.ones((n_nodes))*stations_x[idx] - ssm_x)**2 + \
+                     (np.ones((n_nodes))*stations_y[idx] - ssm_y)**2  
+            #create boolean vector with True for node with the closest node index 
+            # (find the minimum distance between the interested locations 
+            #  and node locations)           
+            closest_node_index=(distance==np.nanmin(distance))
+            # get index where True
+            index.append(np.where(closest_node_index)[0].item())
+            # identify nearest SSM node ID(s) to lat/lon locations 
+            node_id.append(gdf.node_id[closest_node_index].item())
+            
+    except: # for single station location
+        distance=(np.ones((n_nodes))*stations_x - ssm_x)**2 + \
+                  (np.ones((n_nodes))*stations_y - ssm_y)**2 
+        #create boolean vector with True for node with the closest node index 
+        # (find the minimum distance between the interested locations 
+        #  and node locations)
+        closest_node_index=(distance==np.nanmin(distance))
+        # get index where True
+        index.append(np.where(closest_node_index)[0].item())
+        # identify nearest SSM node ID(s) to lat/lon locations 
+        node_id.append(gdf.node_id[closest_node_index].item())
+    return node_id,index,stations_x, stations_y
 
 
 def estimate_nearest_node_DO_NOT_USE(shapefile_path, ilat=48.724,ilon=-122.576):
