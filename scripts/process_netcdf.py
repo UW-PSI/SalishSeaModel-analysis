770bc3be40e2d76a0e65b54e545b925c8a40df23
Rachael D Mueller <rdmseas@uw.edu>
2022-07-11 11:07:27 -0700

    Parallel netcdf processing In this version of the code, I submitted parallel processes via batch script. The code worked...and didn't work. Four of the input files are corrupt and the non-corrupt cases didn't run.  My guess is that I need to add error handling for the corrupted files for this code to not be affected by those cases.

--- scripts/process_netcdf.py
+++ scripts/process_netcdf.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import xarray
 import yaml
 import numpy as np
@@ -8,51 +9,66 @@ import time
 # load functions from my scripts file "ssm_utils"
 from ssm_utils import reshape_fvcom3D, calc_fvcom_stat
 
+def process_netcdf(netcdf_file_path):
+    output_base = pathlib.Path('/mmfs1/gscratch/ssmc/USRS/PSI/Rachael/projects/KingCounty/data')
+    graphics_directory = pathlib.Path('/mmfs1/gscratch/ssmc/USRS/PSI/Rachael/projects/KingCounty/graphics')
 
-output_base = pathlib.Path('/mmfs1/gscratch/ssmc/USRS/PSI/Rachael/projects/KingCounty/data')
-graphics_directory = pathlib.Path('/mmfs1/gscratch/ssmc/USRS/PSI/Rachael/projects/KingCounty/graphics')
+    model_var = 'DOXG'
+    output_dir = output_base/model_var
+    run_type = netcdf_file_path.split('/')[-2]
+    print(run_type)
 
-model_var = 'DOXG'
-output_dir = output_base/model_var
-
-# create output directories if they don't exist for (1) all depths
-if os.path.exists(output_dir)==False:
-    print(f'creating: {output_dir}')
-    os.makedirs(output_dir)
-# bottom depth only
-if os.path.exists(output_dir/'bottom')==False:
-    print(f'creating: {output_dir}/bottom')
-    os.makedirs(output_dir/'bottom')
+    # create output directories if they don't exist for (1) all depths
+    if os.path.exists(output_dir)==False:
+        print(f'creating: {output_dir}')
+        os.makedirs(output_dir)
+        os.makedirs(output_dir/run_type)
+    # bottom depth only
+    if os.path.exists(output_dir/run_type/'bottom')==False:
+        print(f'creating: {output_dir}/{run_type}/bottom')
+        os.makedirs(output_dir/run_type/'bottom')
+        
+    with open('../etc/SSM_config.yaml', 'r') as file:
+        ssm = yaml.safe_load(file)
+    # get shapefile path    
+    shp = ssm['shapefile_path']
+    # load shapefile into geopandas dataframe
+    gdf = gpd.read_file(shp)
+    gdf.head(1)
     
-with open('../etc/SSM_config.yaml', 'r') as file:
-    ssm = yaml.safe_load(file)
-# get shapefile path    
-shp = ssm['shapefile_path']
-# load shapefile into geopandas dataframe
-gdf = gpd.read_file(shp)
-gdf.head(1)
-
-# loop through comparison cases and get timeseries from model output
-#for index,run_type in enumerate(ssm['run_tag']):
-for index,run_type in enumerate([*ssm['run_tag']]):
+    # loop through comparison cases and get timeseries from model output
+    #for index,run_type in enumerate(ssm['run_tag']):
+#for index,run_type in enumerate([*ssm['run_tag']]):
     # input netcdf filename
-    path=pathlib.Path(
-        ssm['output_paths'][index]
-    )
-    print(path.as_posix())
+    path=netcdf_file_path 
+    print('***********************************************************')
+    print('processing: ', path)
     # load variable into xarray and calculate daily min.
-    ds = xarray.open_dataset(path).load()
+    ds = xarray.open_dataset(path,engine='netcdf4')
     dailyDO = reshape_fvcom3D(
         ds[model_var]
     )
     ds.close()
     # calculate daily minimum
     dailyDO_tmin = calc_fvcom_stat(dailyDO, 'min', axis=1)
+
     # store time series of minimum across depth levels & save to file
+    print(f'Saving to file:{run_type}/dailyDO_24hrmin.nc')
     xr_minDO=xarray.DataArray(dailyDO_tmin) 
-    xr_minDO.to_netcdf(output_dir/f'dailyDO_24hrmin_{run_type}.nc')
+    xr_minDO.to_netcdf(output_dir/run_type/f'dailyDO_24hrmin.nc')
     
     # store time series of daily min bottom DO & save to file
     dailyDO_tmin_bottom = dailyDO_tmin[:,-1,:]
+    print(f'Saving to file:{run_type}/bottom/dailyDO_24hrmin_bottom.nc')
     xr_minbotDO=xarray.DataArray(dailyDO_tmin_bottom)
-    xr_minbotDO.to_netcdf(output_dir/'bottom'/f'dailyDO_24hrmin_bottom_{run_type}.nc')
+    xr_minbotDO.to_netcdf(output_dir/run_type/'bottom'/f'dailyDO_24hrmin_bottom.nc')
+
+if __name__=='__main__':
+    args = sys.argv[1:]
+    if os.path.exists(args[0]):
+        process_netcdf(args[0])
+    else:
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), args[0]
+            )
+
--- scripts/process_netcdf.sh
+++ scripts/process_netcdf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 ## job name 
-#SBATCH --job-name=process_netcdf
+#SBATCH --job-name=SSM_netcdf
 #SBATCH --account=ssmc
 #SBATCH --partition=compute
-#SBATCH --nodes=1       
-#SBATCH --ntasks-per-node=1 
-#SBATCH --time=2:00:00 
+#SBATCH --nodes=7       
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=5:00:00 
 #SBATCH --mem=175G 
 #SBATCH --mail-user=rdmseas@uw.edu
 
@@ -19,8 +19,14 @@ conda activate klone_jupyter
 echo  starting the run
 date
 
-python process_netcdf.py
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/wqm_baseline/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/1b_all_sog_wwtp_off/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/1c_all_sog_riv_off/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/1d_small_sog_wwtp_off/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/1e_med_sog_wwtp_off/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/2a_sog_river_0.5times/ssm_output.nc" &
+python process_netcdf.py "/mmfs1/gscratch/ssmc/USRS/PSI/Sukyong/kingcounty/2b_sog_river_2times/ssm_output.nc" &
 
 date
 echo run ended
- 
+
