357e0ee178f8d1cb6d0a51345b6142d929ace92b
Rachael D. Mueller <59846131+RachaelDMueller@users.noreply.github.com>
2023-06-01 14:04:12 -0700

    Update ssm_utils.py

--- py_scripts/ssm_utils.py
+++ py_scripts/ssm_utils.py
@@ -1,4 +1,4 @@
-# Created by Rachael D. Mueller at the Puget Sound Institute
+# Created by Rachael D. Mueller at the Puget Sound Institute with funding from King County
 import numpy
 import geopandas as gpd
 from shapely.geometry import Point
